550937b0e35a970c3f7a2affb28c8a98660630ac
JoseesBronner <joseesbronner@gmail.com>
2021-02-10 20:53:49 -0500

    commit tree 75aad46951088668eb62cd14181c11f9db555ebe

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,22 @@
-def unsafe?(speed)
+def unsafe?(speed) 
+  if speed < 40 || speed > 60 
+     return true
+  else 
+    return false 
+    end
 
+puts unsafe?(79) 
+puts unsafe?(35) 
+puts unsafe?(50)
+
+
+
+def not_safe(speed)
+  return ((speed < 40 || speed > 60) ? true : false)
 end
 
+return not_safe(79)
+return not_safe(35)
+return not_safe(50)	
 
-
-def not_safe?(speed)
-	
 end
-	
-
-
